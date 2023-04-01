fc7d18b0848bcad75cc72eb9f24bdba84fd14496
tips4you <40390368+tips4you@users.noreply.github.com>
2023-04-01 18:46:47 -0400

    Update app.js

--- section1/app.js
+++ section1/app.js
@@ -14,21 +14,21 @@ const msg = getMyData(5, 12);
 
 console.log(msg);
 
-//Class 3
+//Class 3 : Export single function and values from a module by either using module.exports
 const getSubRes = require("./method");
 
 const msg = getSubRes(15, 9);
 
 console.log(msg);
 
-//Class 4: validator
+//Class 4: A library of string validators and sanitizers.
 const val = require("validator");
 
 console.log(val.isEmail("varun123@gmail.com"));
 
 console.log(val.isURL("http://google.com"));
 
-//Class 5: Pretty Output
+//Class 5: To style terminal strings : npm install chalk@4.1.2
 const val = require("chalk");
 
 const msg = val.red.inverse.bold("Success!");
@@ -37,7 +37,7 @@ console.log(msg);
 
 console.log(val.blue.inverse.bold("Hello") + " World" + val.red("!"));
 
-//Class 6
+//Class 6 : Export functions and values from a module by either using module.exports
 const getSubRes = require("./method");
 
 const msg = getSubRes.getSubRes(15, 9);
