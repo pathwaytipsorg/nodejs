da73b9450a21f18120973536f67ce1143b689a36
tips4you <40390368+tips4you@users.noreply.github.com>
2023-04-01 18:58:36 -0400

    Update app.js

--- section1/app.js
+++ section1/app.js
@@ -1,11 +1,11 @@
-// Class 1 : fs: writing data to file
+// Class 1 : fs module to write data to a file (*.out, *.csv, *.txt)
 const abc = require("fs");
 
 f.writeFileSync("firstExample.out", "Hello");
 
 abc.appendFileSync("firstExample.out", " testing my abc const");
 
-//Class 2 : Return Method
+//Class 2 : Return Method : To specify the value that a function should return.
 const getMyData = function (a, b) {
   return a - b;
 };
@@ -22,13 +22,15 @@ const msg = getSubRes(15, 9);
 console.log(msg);
 
 //Class 4: A library of string validators and sanitizers.
+//npm i validator@13.9.0
 const val = require("validator");
 
 console.log(val.isEmail("varun123@gmail.com"));
 
 console.log(val.isURL("http://google.com"));
 
-//Class 5: To style terminal strings : npm install chalk@4.1.2
+//Class 5: To style terminal strings 
+//npm install chalk@4.1.2
 const val = require("chalk");
 
 const msg = val.red.inverse.bold("Success!");
