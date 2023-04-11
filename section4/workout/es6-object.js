62d5cd9b3384d2d28e7594afaa92cdfcf747aa37
tips4you <40390368+tips4you@users.noreply.github.com>
2023-04-11 18:27:50 -0400

    Create es6-object.js

---
+++ section4/workout/es6-object.js
@@ -0,0 +1,27 @@
+const firstName = "Tom";
+const lastName = "Ran";
+
+const emp = {
+  firstName,
+  LastName: lastName,
+  age: 13,
+  salary: undefined,
+};
+
+console.log(emp);
+
+//Object destructuring
+const lName = emp.LastName;
+
+console.log("Last Name is ", lName);
+
+const { salary = 2000, age: EmpAge } = emp;
+
+console.log("Age is " + EmpAge + " and salary is " + salary);
+
+//
+const transaction = (type, { firstName, age }) => {
+  console.log("Transaction is for " + firstName + " and age is " + age);
+};
+
+transaction("newEntry", emp);
