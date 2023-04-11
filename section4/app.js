2930baf7b29d5c2b49fa1c64917d1f0f1eaff5f8
tips4you <40390368+tips4you@users.noreply.github.com>
2023-04-11 18:38:56 -0400

    Update app.js

--- section4/app.js
+++ section4/app.js
@@ -1,8 +1,13 @@
 const weatherModule = require("./utils/weatherModule");
 
 const zip = process.argv[2];
-//zip = "M6E2Z2"
-weatherModule(zip, (err, data) => {
-  console.log("Error: ", err);
-  console.log("Data: ", data);
-});
+
+if (!zip) {
+  console.log("Please provide zipcode!");
+} else {
+  weatherModule(zip, (err, data) => {
+    if (err === undefined) {
+      console.log("Data: ", data);
+    }
+  });
+}
