b26691473d0a4965d3e5b1eb181c3ec2ebad0f7c
tips4you <40390368+tips4you@users.noreply.github.com>
2023-04-14 18:09:58 -0400

    Update es6-object.js

--- section4/workout/es6-object.js
+++ section4/workout/es6-object.js
@@ -20,7 +20,7 @@ const { salary = 2000, age: EmpAge } = emp;
 console.log("Age is " + EmpAge + " and salary is " + salary);
 
 //
-const transaction = (type, { firstName, age }={}) => {
+const transaction = (type, { firstName, age=0 }={}) => {
   console.log("Transaction is for " + firstName + " and age is " + age);
 };
 
