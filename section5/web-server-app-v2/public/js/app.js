b57915e507f8257bd5732cae3c5896a396e01012
tips4you <40390368+tips4you@users.noreply.github.com>
2023-04-26 14:46:04 -0400

    Update app.js

--- section5/web-server-app-v2/public/js/app.js
+++ section5/web-server-app-v2/public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/zip?zip=" + location).then((response) => {
+  fetch("weather?zip=" + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
