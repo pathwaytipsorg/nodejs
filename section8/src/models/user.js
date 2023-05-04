53513984217aa61f76fe91fb1f3670eed7837c07
tips4you <40390368+tips4you@users.noreply.github.com>
2023-05-04 19:25:33 -0400

    Create user.js

---
+++ section8/src/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a postive number");
+      }
+    },
+  },
+});
+
+module.exports = User;
